d0b205993560228e91f72ca8772e972fb1c0a9f3
Roni Nevalainen <kitten@kittenz.dev>
2022-08-12 19:09:01 +0300

    Fix CORS and make the image 2:1 aspect ratio

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, send_file
+from flask import Flask, jsonify, render_template, send_file, request
 import io
 import math
 import mimetypes
@@ -15,6 +15,16 @@ from lookaround import get_coverage_tile, fetch_pano_segment
 from util import CustomJSONEncoder
 from geo import haversine_distance
 
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 def create_app():
     app = Flask(__name__)
@@ -56,19 +66,22 @@ def create_app():
     
     @app.route("/closest/<float(signed=True):lat>/<float(signed=True):lon>/")
     def closest_pano_to_coord(lat, lon):
-        x, y = wgs84_to_tile_coord(lat, lon, 17)
-        panos = get_coverage_tile(x, y)
-        if len(panos) == 0:
-            return jsonify(None)
+        if request.method == "OPTIONS": # CORS preflight
+            return _build_cors_preflight_response()
+        else:
+            x, y = wgs84_to_tile_coord(lat, lon, 17)
+            panos = get_coverage_tile(x, y)
+            if len(panos) == 0:
+                return jsonify(None)
 
-        smallest_distance = 9999999
-        closest = None
-        for pano in panos:
-            distance = haversine_distance(lat, lon, pano.lat, pano.lon)
-            if distance < smallest_distance:
-                smallest_distance = distance
-                closest = pano
-        return jsonify(closest)
+            smallest_distance = 9999999
+            closest = None
+            for pano in panos:
+                distance = haversine_distance(lat, lon, pano.lat, pano.lon)
+                if distance < smallest_distance:
+                    smallest_distance = distance
+                    closest = pano
+            return _corsify_actual_response(jsonify(closest))
 
     @app.route("/pano/<int:panoid>/<int:region_id>/<int:zoom>/")
     def relay_full_pano(panoid, region_id, zoom):
@@ -82,11 +95,11 @@ def create_app():
         WIDTH_SIZE = round(heic_array[0].width * (1024 / 5632))
         widths, heights = zip(*(i.size for i in heic_array))
         total_width, max_height = (sum(widths)-WIDTH_SIZE), max(heights)
-        heic_pano = Image.new('RGB', (total_width, max_height))
-        heic_pano.paste(heic_array[0], (0,0))
-        heic_pano.paste(heic_array[1], (heic_array[0].width-TILE_SIZE, 0))
-        heic_pano.paste(heic_array[2], ((heic_array[0].width+heic_array[1].width)-(TILE_SIZE*2), 0))
-        heic_pano.paste(heic_array[3], ((heic_array[0].width+heic_array[1].width+heic_array[2].width)-(TILE_SIZE*3), 0))
+        heic_pano = Image.new('RGB', (total_width, 8192))
+        heic_pano.paste(heic_array[0], (0,round((8192-max_height)/2)))
+        heic_pano.paste(heic_array[1], (heic_array[0].width-TILE_SIZE, round((8192-max_height)/2)))
+        heic_pano.paste(heic_array[2], ((heic_array[0].width+heic_array[1].width)-(TILE_SIZE*2), round((8192-max_height)/2)))
+        heic_pano.paste(heic_array[3], ((heic_array[0].width+heic_array[1].width+heic_array[2].width)-(TILE_SIZE*3), round((8192-max_height)/2)))
         
         with io.BytesIO() as output:
             heic_pano.save(output, format="jpeg")
