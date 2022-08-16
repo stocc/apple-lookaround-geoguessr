81d22138c7aaee2aaf6f7343edb7eabaf8b8b652
Roni Nevalainen <kitten@kittenz.dev>
2022-08-16 21:49:36 +0300

    feat: add webpack

--- userscript/src/index.ts
+++ userscript/src/index.ts
@@ -1,3 +1,17 @@
+// ==UserScript==
+// @name         AppleGuessr
+// @namespace    https://greasyfork.org/en/users/946023-mistystar
+// @version      1.0
+// @description  Adds Apple Look Around to GeoGuessr
+// @author       Mistystar (Mistystar#2205 on Discord, https://www.youtube.com/channel/UC4IHxYw9Aoz8cf9BIdHKd3A on YT)
+// @match        https://www.geoguessr.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
+// @grant        none
+// @license      MIT
+// @run-at       document-start
+// @require 	 https://cdnjs.cloudflare.com/ajax/libs/heic2any/0.0.3/heic2any.min.js
+
+// ==/UserScript==
 /*
 CREDITS
 
@@ -425,3 +439,5 @@ function onLoad() {
 (function() {
 	onLoad();
 })();
+
+window.onload = onLoad;
