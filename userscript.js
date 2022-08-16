94979ab3b599071f69f76d30d158bc34144d0687
Daniel Petri <daniel.petri@rwth-aachen.de>
2022-08-16 15:44:56 +0200

    Hosted current backend, Unity CORS Proxy

--- userscript.js
+++ userscript.js
@@ -28,7 +28,11 @@ Massive thank you to the following people:
 // 0 best, 4 worst
 const resolutionSetting = 2;
 const headingCalibration = 40;
+const BASE_URL = "https://lookaround-alpha.herokuapp.com/"
+// docker run --publish 8080:8080 testcab/cors-anywhere
+//const CORS_PROXY = "http://localhost:8080/"
 
+const CORS_PROXY = "https://nameless-bastion-28139.herokuapp.com/"
 
 const extensionFactor = 2; // TODO Play around with this value for best results with image stretching
 
@@ -40,7 +44,6 @@ async function blobToBase64(blob) {
 	});
   }
 
-const BASE_URL = "http://127.0.0.1:5001/"
 
 const MENU_HTML = `
 <div class="start-standard-game_settings__x94PU">
@@ -272,8 +275,7 @@ async function loadTileForPano(panoFullId, x) {
         //var panoURL = "https://cors-anywhere.herokuapp.com/"+parsed.url;
 
 		// CORS Proxy running locally
-		// docker run --publish 8080:8080 testcab/cors-anywhere
-		var panoURL = "http://localhost:8080/"+parsed.url;
+		var panoURL = CORS_PROXY+parsed.url;
 		//var panoURL = parsed.url;
 
 		console.log("Requesting tile " + [x, parsed.url, panoURL])
