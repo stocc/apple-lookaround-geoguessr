e7d7dc48619c5867fe4ecda19989febdbf53c8f6
Daniel Petri <daniel.petri@rwth-aachen.de>
2022-08-16 20:14:47 +0200

    Port new features to TypeScript

--- userscript/src/options.ts
+++ userscript/src/options.ts
@@ -13,14 +13,89 @@
 
 // ==/UserScript==
 
-const RESOLUTION_SETTING = 2; // 0 best, 4 worst
+
+// Determines the resolution of images requested from Apple
+// Setting a higher resolution will make rounds load WAY slower, until browsers start to support HEIC
+// 0 = highest resolution available, 4 = lowest resolution available.
+// Default: 2
+const RESOLUTION_SETTING = 2;
+
+// Constant value added to calculated heading to calibrate the GeoGuessr compass
+const HEADING_CALIBRATION = 40;
+
 
 const EXTENSION_FACTOR = 2; // TODO Play around with this value for best results with image stretching
 
-const BASE_URL = "http://127.0.0.1:5001/";
+// Python Backend API URL
+const BASE_URL = "https://lookaround-alpha.herokuapp.com/"
+const CORS_PROXY = "https://nameless-bastion-28139.herokuapp.com/"
+
+
+const RESOLUTION_PROFILES = {
+	0: {
+		"overlap": 256,
+		"big": {
+			"width": 5632,
+			"height": 4352,
+		},
+		"small": {
+			"width": 3072,
+			"height": 4352,
+		}
+	},
+	1: {
+		"overlap": 188,
+		"big": {
+			"width": 4128,
+			"height": 3088,
+		},
+		"small": {
+			"width": 2256,
+			"height": 3088,
+		},
+	},
+	2: {
+		"overlap": 100,
+		"big": {
+			"width": 2208,
+			"height": 1648,
+		},
+		"small": {
+			"width": 1200,
+			"height": 1648,
+		}
+	},
+	3: {
+		"overlap": 71,
+		"big": {
+			"width": 1568,
+			"height": 1168,
+		},
+		"small": {
+			"width": 848,
+			"height": 1168,
+		}
+	},
+	4: {
+		"overlap": 50,
+		"big": {
+			"width": 1104,
+			"height": 832,
+		},
+		"small": {
+			"width": 608,
+			"height": 832,
+		}
+	}
+
+}
+
 
 export {
 	RESOLUTION_SETTING,
+	HEADING_CALIBRATION,
 	EXTENSION_FACTOR,
 	BASE_URL,
+	CORS_PROXY,
+	RESOLUTION_PROFILES
 };
